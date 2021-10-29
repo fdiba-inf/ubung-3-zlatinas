410302571b14c40befa47512b457d9da5506e42a
zlatinas <>
2021-10-29 10:56:06 +0000

    z

--- src/exercise3/Triangle.java
+++ src/exercise3/Triangle.java
@@ -1,41 +1,44 @@
 package exercise3;
 
 import java.util.Scanner;
+import java.lang.Math;
 
-  public class Triangle {
-  public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
+public class Triangle {
 
-    double a, b, c, s;
-    double p= 0;
-     
-    do {
-    a = input.nextDouble();
-    b = input.nextDouble();
-    c = input.nextDouble();
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double a, b, c, p, s;
+        
+        do {
+          a = input.nextDouble();
+          b = input.nextDouble();
+          c = input.nextDouble();
 
-    if (a + b < c && b + c < a && a +c > b && a > 0 && b > 0 && c > 0)   {
-     System.out.println("Values are not correct!");
-    
-    } else  
-    p = (a + b + c)/2;
-    s = Math.sqrt( p *(p - a)*(p - b)*(p - c));
-    System.out.println("Area: " + s);
+          if ((a + b <= c) || (a + c <= b) || (b + c <= a)) {
+            System.out.println ("Values are not correct!");
 
+          } else {
+            p = (a + b + c) / 2;
+            s = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+            System.out.println("Area: " + s);
 
-    if ( a == b || b == c || a == c) {
+            if (a == b && a == c) {
+              System.out.println("Triangle: equilateral");
 
-    System.out.println("Triangle: isosceles");
+            }  else if (a == b && a != c) {
+                System.out.println("Triangle: isosceles");
 
-    } else if ( a== b && b == c) {
-      System.out.println("Triangle: equilateral");
+             }   else if ( a == c && a != b) {
+                  System.out.println("Triangle: isosceles");
 
-    } else  { 
-      System.out.println("Triangle: scalene");
+              }    else if (c == b && c != a) {
+                    System.out.println("Triangle: isosceles");
 
+              } else {
+                System.out.println("Triangle: scalene");
+
+            }
+         }
+       } while (a > 0 && b > 0 && c > 0);
     }
-  
-    } while ( a > 0 && b > 0 && c > 0); 
-
-  }
- }
+}
